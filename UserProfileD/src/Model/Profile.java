b886e3e67fee5a4602be157466285ac5b2d51372
RanjithKumar839 <sepoy.s@northeastern.edu>
2022-09-21 01:21:30 -0400

    Assignment_1

---
+++ UserProfileD/src/Model/Profile.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author ranji
+ */
+public class Profile {
+    
+    private String FirstName;
+    private String LastName;
+    private String DOB;
+    private String Age;
+    private String Telephone;
+    private String Email;
+    private String StreetLine1;
+    private String StreetLine2;
+    private String City;
+    private String Country;
+    private String Affiliation;
+    private String Major;
+    private String Career;
+    private String Degree1;
+    private String Degree1StartDate;
+    private String Degree1EndDate;
+    private String Degree2;
+    private String Degree2StartDate;
+    private String Degree2EndDate;
+    private Image Photo;
+
+    public Image getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(Image Photo) {
+        this.Photo = Photo;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStreetLine1() {
+        return StreetLine1;
+    }
+
+    public void setStreetLine1(String StreetLine1) {
+        this.StreetLine1 = StreetLine1;
+    }
+
+    public String getStreetLine2() {
+        return StreetLine2;
+    }
+
+    public void setStreetLine2(String StreetLine2) {
+        this.StreetLine2 = StreetLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public String getDegree1() {
+        return Degree1;
+    }
+
+    public void setDegree1(String Degree1) {
+        this.Degree1 = Degree1;
+    }
+
+    public String getDegree1StartDate() {
+        return Degree1StartDate;
+    }
+
+    public void setDegree1StartDate(String Degree1StartDate) {
+        this.Degree1StartDate = Degree1StartDate;
+    }
+
+    public String getDegree1EndDate() {
+        return Degree1EndDate;
+    }
+
+    public void setDegree1EndDate(String Degree1EndDate) {
+        this.Degree1EndDate = Degree1EndDate;
+    }
+
+    public String getDegree2() {
+        return Degree2;
+    }
+
+    public void setDegree2(String Degree2) {
+        this.Degree2 = Degree2;
+    }
+
+    public String getDegree2StartDate() {
+        return Degree2StartDate;
+    }
+
+    public void setDegree2StartDate(String Degree2StartDate) {
+        this.Degree2StartDate = Degree2StartDate;
+    }
+
+    public String getDegree2EndDate() {
+        return Degree2EndDate;
+    }
+
+    public void setDegree2EndDate(String Degree2EndDate) {
+        this.Degree2EndDate = Degree2EndDate;
+    }
+    
+    
+}
